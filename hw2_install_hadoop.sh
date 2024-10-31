66d4fcd793251487a78cb79d56899c6d2b50091d
Timur Ermeshev <ermetim@gmail.com>
2024-11-01 00:45:04 +0300

    Add config files

--- hw2_install_hadoop.sh
+++ hw2_install_hadoop.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Запрашиваем пароли
+# Запрашиваем SSH пароль
 read -s -p "Введите SSH пароль: " SSH_PASS
-echo
-NEW_USER="hadoop"
 echo
 
 # Определяем переменные
-JUMP_SERVER="team@176.109.91.41"
+NEW_USER="hadoop"
+
+# Определяем IP-адреса целевых нод
 IP_NODES=(
     "192.168.1.30"
     "192.168.1.31"
@@ -15,88 +15,88 @@ IP_NODES=(
     "192.168.1.33"
 )
 
-PUBLIC_KEYS="# new keys for $NEW_USER"
-
-# Формируем массив TARGET_NODES на основе IP_NODES
-TARGET_NODES=()
+# Формируем массив USER_NODES на основе IP_NODES
+USER_NODES=()
 for IP in "${IP_NODES[@]}"; do
-    TARGET_NODES+=("team@$IP")
+    USER_NODES+=("$NEW_USER@$IP")
 done
 
-# Данные для записи в /etc/hosts (восстановление)
-HOSTS_DATA="127.0.0.1 localhost
-127.0.1.1 team-7-nn
+HADOOP_VERSION="3.4.0"
+HADOOP_TAR="hadoop-$HADOOP_VERSION.tar.gz"
+HADOOP_URL="https://dlcdn.apache.org/hadoop/common/hadoop-$HADOOP_VERSION/$HADOOP_TAR"
 
-# The following lines are desirable for IPv6 capable hosts
-::1     ip6-localhost ip6-loopback
-fe00::0 ip6-localnet
-ff00::0 ip6-mcastprefix
-ff02::1 ip6-allnodes
-ff02::2 ip6-allrouters"
+# Директория с конфигурационными файлами
+CONFIG_DIR="bigdata_team-7/config_files"
 
-# Функция для возврата системы в прежнее состояние
-restore_system() {
-    local NODE="$1"
-    echo "Подключаемся к $NODE для восстановления..."
-
-    sshpass -p "$SSH_PASS" ssh -J "$JUMP_SERVER" "$NODE" bash << EOF
-
-    # Удаляем пользователя hadoop, если он существует
-    if getent passwd "$NEW_USER" > /dev/null; then
-        echo "$SSH_PASS" | sudo -S -p "" deluser --remove-home "$NEW_USER"
-        echo "Пользователь $NEW_USER удален."
-    fi
-
-    # Удаляем папку hadoop-3.4.0, если она существует
-    if [ -d "hadoop-3.4.0" ]; then
-        echo "Удаляем папку hadoop-3.4.0..."
-        rm -rf "hadoop-3.4.0"
-    fi
-
-    # Очищаем файл /etc/hosts
-    echo "$SSH_PASS" | sudo -S bash -c 'echo -n "" > /etc/hosts'
-
-    # Записываем данные хостов в /etc/hosts
-    echo "$SSH_PASS" | sudo tee -a /etc/hosts > /dev/null << HOSTS
-$HOSTS_DATA
-HOSTS
-
-    echo "Файл /etc/hosts восстановлен на $NODE."
-EOF
-
-    echo "*****************************************************************************"
-    echo
-}
-
-# Копируем конфигурационные файлы и перемещаем их
-copy_config_files() {
-    local NODE="$1"
-
-    echo "Копируем конфигурационные файлы на $NODE..."
-
-    # Копируем файл nn
-    sshpass -p "$SSH_PASS" scp bigdata_team-7/config_files/nn "$NODE:~/nn"
-    # Перемещаем файл в /etc/nginx/sites-enabled с правами суперпользователя
-    sshpass -p "$SSH_PASS" ssh "$NODE" bash << EOF
-        echo "$SSH_PASS" | sudo -S mv ~/nn /etc/nginx/sites-enabled/
-        echo "Файл nn перемещен в /etc/nginx/sites-enabled на $NODE."
-EOF
-
-    # Копируем и заменяем остальные конфигурационные файлы
-    for file in .profile core-site.xml hadoop-env.sh hdfs-site.xml workers; do
-        echo "Копируем и заменяем файл $file на $NODE..."
-        sshpass -p "$SSH_PASS" scp "bigdata_team-7/config_files/$file" "$NODE:~/$file"
-        sshpass -p "$SSH_PASS" ssh "$NODE" bash << EOF
-            echo "$SSH_PASS" | sudo -S mv ~/$file ~/hadoop-3.4.0/etc/hadoop/$file
-            echo "Файл $file перемещен в ~/hadoop-3.4.0/etc/hadoop/ на $NODE."
-EOF
+# Функция для установки утилит
+install_utilities() {
+    for utility in "$@"; do
+        if ! command -v "$utility" &> /dev/null; then
+            echo "$utility не установлен. Устанавливаем..."
+            echo "$SSH_PASS" | sudo -S apt update -y
+            echo "$SSH_PASS" | sudo -S apt install -y "$utility"
+        else
+            echo "$utility уже установлен."
+        fi
     done
 }
 
-# Цикл по всем целевым нодам для восстановления и копирования конфигурационных файлов
-for NODE in "${TARGET_NODES[@]}"; do
-    restore_system "$NODE"
-    copy_config_files "$NODE"
+# Установка необходимых утилит
+install_utilities sshpass wget tar
+
+# Подключаемся к первой ноде и скачиваем Hadoop, если его нет
+USER_JN="${USER_NODES[0]}"
+USER_NN="${USER_NODES[1]}"
+echo "Подключаемся к $USER_JN и проверяем наличие архива $HADOOP_TAR..."
+
+sshpass -p "$SSH_PASS" ssh "$USER_JN" bash << EOF
+    if [[ ! -f "$HADOOP_TAR" ]]; then
+        echo "Архив $HADOOP_TAR не найден. Скачиваем Hadoop..."
+        wget "$HADOOP_URL"
+
+        if [[ -f "$HADOOP_TAR" ]]; then
+            echo "Hadoop успешно скачан на $USER_JN."
+        else
+            echo "Ошибка при скачивании Hadoop. Завершаем выполнение."
+            exit 1
+        fi
+    else
+        echo "Архив $HADOOP_TAR уже существует на $USER_JN, пропускаем скачивание."
+    fi
+EOF
+
+# Копируем архив на остальные ноды только если файла еще нет
+for NODE in "${USER_NODES[@]:1}"; do
+    echo "Копируем Hadoop на $NODE..."
+    sshpass -p "$SSH_PASS" scp "$USER_JN:$HADOOP_TAR" "$NODE:~"
+
+    echo "Распаковываем Hadoop на $NODE..."
+    sshpass -p "$SSH_PASS" ssh "$NODE" bash << EOF
+        if [ -f ~/$HADOOP_TAR ]; then
+            if [ -d "hadoop-$HADOOP_VERSION" ]; then
+                echo "Папка hadoop-$HADOOP_VERSION уже существует. Удаляем..."
+                rm -rf "hadoop-$HADOOP_VERSION"
+            fi
+            tar -xvzf ~/$HADOOP_TAR
+            echo "Hadoop успешно установлен на $NODE."
+            rm -f ~/$HADOOP_TAR  # Удаляем архив после распаковки
+        else
+            echo "Архив $HADOOP_TAR не найден на $NODE."
+        fi
+EOF
+
+    # Копируем конфигурационные файлы
+    echo "Копируем конфигурационные файлы на $NODE..."
+    sshpass -p "$SSH_PASS" scp "$CONFIG_DIR/.profile" "$NODE:~/.profile"
+    sshpass -p "$SSH_PASS" scp "$CONFIG_DIR/core-site.xml" "$NODE:~/hadoop-$HADOOP_VERSION/etc/hadoop/core-site.xml"
+    sshpass -p "$SSH_PASS" scp "$CONFIG_DIR/hadoop-env.sh" "$NODE:~/hadoop-$HADOOP_VERSION/etc/hadoop/hadoop-env.sh"
+    sshpass -p "$SSH_PASS" scp "$CONFIG_DIR/hdfs-site.xml" "$NODE:~/hadoop-$HADOOP_VERSION/etc/hadoop/hdfs-site.xml"
+    sshpass -p "$SSH_PASS" scp "$CONFIG_DIR/nn" "$NODE:/etc/nginx/sites-enabled/nn"
+    sshpass -p "$SSH_PASS" scp "$CONFIG_DIR/workers" "$NODE:~/hadoop-$HADOOP_VERSION/etc/hadoop/workers"
+
+    echo "Конфигурационные файлы успешно скопированы на $NODE."
+    echo "*****************************************************************************"
+    echo
 done
 
 echo "Скрипт выполнен успешно."
